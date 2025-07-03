16396e1b767df93f0c315cf6b6b54fb89c6081d4
Shen Chen Xu <shenchenxu@gmail.com>
2025-07-03 15:10:32 -0700

    Initial commit with some keymaps.

---
+++ lua/user/keymaps.lua
@@ -0,0 +1,9 @@
+vim.keymap.set('n', '<Space>', '10j')
+vim.keymap.set('n', '<Backspace>', '10k')
+vim.keymap.set('n', '<C-H>', '<C-W>h')
+vim.keymap.set('n', '<C-J>', '<C-W>j')
+vim.keymap.set('n', '<C-K>', '<C-W>k')
+vim.keymap.set('n', '<C-L>', '<C-W>l')
+vim.keymap.set('n', '<C-N>', ':bnext<CR>')
+vim.keymap.set('n', '<C-P>', ':bprevious<CR>')
+
