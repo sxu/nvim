a2e783c942988ba4261e02691108f7707bcc0ee7
Shen Chen Xu <shenchenxu@gmail.com>
2025-07-07 16:49:56 -0700

    More.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,6 +4,18 @@ return {
     branch = "master",
     lazy = false,
     build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "cpp",
+          "lua",
+          "markdown",
+          "python",
+          "vim",
+          "vimdoc",
+        }
+      })
+    end,
   },
   {
     "numToStr/Comment.nvim",
