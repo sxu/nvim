154f0e97aee2297fc35e20df17da5841f077a593
Shen Chen Xu <shenchenxu@gmail.com>
2025-07-17 08:07:11 -0700

    More.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -35,5 +35,11 @@ return {
         show_end = false,
       },
     },
+    config = function(_, opts)
+      require("ibl").setup(opts)
+      local hooks = require("ibl.hooks")
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+    end,
   },
 }
