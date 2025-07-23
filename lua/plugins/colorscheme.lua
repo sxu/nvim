e749a16f7c8b51aeee2285e63391a9526235b90e
Shen Chen Xu <shenchenxu@gmail.com>
2025-07-22 20:17:30 -0700

    More.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,26 +5,17 @@ return {
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_foreground = "mixed"
     end,
-    config = function()
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
   },
   {
     "sainnhe/everforest",
     init = function()
       vim.g.everforest_background = "hard"
     end,
-    -- config = function()
-    --   vim.cmd.colorscheme("everforest")
-    -- end,
   },
   {
     "sainnhe/sonokai",
     init = function()
       vim.g.sonokai_style = "default"
     end,
-    -- config = function()
-    --   vim.cmd.colorscheme("sonokai")
-    -- end,
   },
 }
