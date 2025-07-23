20abc907b4f0300b7e83d7d44489d900e5450857
Shen Chen Xu <shenchenxu@gmail.com>
2025-07-22 20:16:39 -0700

    More.

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -25,21 +25,21 @@ return {
     "numToStr/Comment.nvim",
     opts = {},
   },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    opts = {
-      indent = { char = "▏" },
-      scope = {
-        show_start = false,
-        show_end = false,
-      },
-    },
-    config = function(_, opts)
-      require("ibl").setup(opts)
-      local hooks = require("ibl.hooks")
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
-      hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
-    end,
-  },
+  -- {
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   main = "ibl",
+  --   opts = {
+  --     indent = { char = "▏" },
+  --     scope = {
+  --       show_start = false,
+  --       show_end = false,
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     require("ibl").setup(opts)
+  --     local hooks = require("ibl.hooks")
+  --     hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+  --     hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+  --   end,
+  -- },
 }
