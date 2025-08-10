c189cb53490c643b074cce9cb914fe5e2280b6fa
Shen Chen Xu <shenchenxu@gmail.com>
2025-08-09 18:30:49 -0700

    Some colorscheme and UI updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,19 +4,26 @@ return {
     init = function()
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_foreground = "material"
+      vim.g.gruvbox_material_enable_italic = true
     end,
   },
   {
     "sainnhe/everforest",
     init = function()
       vim.g.everforest_background = "hard"
+      vim.g.everforest_enable_italic = true
     end,
   },
   {
     "sainnhe/sonokai",
     init = function()
       vim.g.sonokai_style = "default"
+      vim.g.sonokai_enable_italic = true
     end,
   },
   { "EdenEast/nightfox.nvim" },
+  {
+    "ribru17/bamboo.nvim",
+    opts = { style = "multiplex" },
+  },
 }
