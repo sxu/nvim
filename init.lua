f186af1070e1134abb1e454ab97fe754e63ad474
Shen Chen Xu <shenchenxu@gmail.com>
2025-08-13 18:15:01 -0700

    Fix cursor

--- init.lua
+++ init.lua
@@ -2,3 +2,10 @@ require("user.options")
 require("user.plugins")
 require("user.keymaps")
 vim.cmd.colorscheme("everforest")
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "*",
+  callback = function()
+    vim.opt.guicursor = ""
+    vim.fn.chansend(vim.v.stderr, "\x1b[ q")
+  end,
+})
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -5,6 +5,7 @@ vim.g.maplocalleader = "\\"
 opt.background = "dark"
 opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 opt.foldmethod = "expr"
+opt.guicursor = "n-v-c-sm:block,i-ci-ve:block-blinkon500-blinkoff500,r-cr-o:hor20,t:block-blinkon500-blinkoff500-TermCursor"
 opt.ignorecase = true
 opt.mouse = ""
 opt.number = true
